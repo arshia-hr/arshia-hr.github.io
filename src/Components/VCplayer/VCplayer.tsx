cb024a7d79409aa2f3773225e0b39c1a209b9062
arshia-hr <arshiahvgrdi@gmail.com>
2024-10-17 12:32:39 -0700

    add thumbnail for video

--- src/Components/VCplayer/VCplayer.tsx
+++ src/Components/VCplayer/VCplayer.tsx
@@ -2,7 +2,7 @@ import react from "../../assets/how-to-use-svgs-react.avif";
 function VCplayer() {
   return (
     <div>
-      <video className="w-full" src={react} controls muted defaultValue={1}></video>
+      <video className="w-full" poster={react} controls muted defaultValue={1}></video>
     </div>
   );
 }
